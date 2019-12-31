3cd5c8d933902d482c6313615ab447be502f0bd4
punitchaudhari <58459971+punitchaudhari@users.noreply.github.com>
2019-12-31 17:22:23 +0530

    added elements

--- src/main/java/com/AutomationFramework/companyName/automationEcommerce/pageObject/Page_aboutUs.java
+++ src/main/java/com/AutomationFramework/companyName/automationEcommerce/pageObject/Page_aboutUs.java
@@ -12,20 +12,29 @@ import com.aventstack.extentreports.Status;
 
 public class Page_aboutUs {
 
-	
 	public WebDriver driver;
 	Logger log = LoggerHelper.getLogger(Page_aboutUs.class);
-	
-	public Page_aboutUs(WebDriver driver){
+
+	public Page_aboutUs(WebDriver driver) {
 		this.driver = driver;
-		
 		PageFactory.initElements(driver, this);
 		log.info("Page_aboutUs Class Object Created");
-		TestBase.test.log(Status.INFO,"Page_aboutUs Class Object Created");	
+		TestBase.test.log(Status.INFO, "Page_aboutUs Class Object Created");
 	}
-	
-	@FindBy(xpath="")
-	public WebElement ;
-	
-}
 
+	@FindBy(xpath = "//*[@id='columns']/div[1]/a")
+	public WebElement img_homeIcon;
+
+	@FindBy(xpath = "//*[@id='center_column']/div/h1")
+	public WebElement lbl_aboutUs;
+
+	@FindBy(xpath = "//*[@id='center_column']/div/div/div[1]/div/h3")
+	public WebElement lbl_ourCompany;
+
+	@FindBy(xpath = "//*[@id='center_column']/div/div/div[2]/div/h3")
+	public WebElement lbl_ourTeam;
+
+	@FindBy(xpath = "//*[@id='center_column']/div/div/div[3]/div/h3")
+	public WebElement lbl_testimonials;
+
+}
