ddd55fcc182c6867e4c938535a0514c3c4f7cdf2
punitchaudhari <58459971+punitchaudhari@users.noreply.github.com>
2020-01-21 13:28:05 +0530

    added elements and methods for Page_newProducts.java

--- src/main/java/com/AutomationFramework/companyName/automationEcommerce/pageObject/Page_newProducts.java
+++ src/main/java/com/AutomationFramework/companyName/automationEcommerce/pageObject/Page_newProducts.java
@@ -2,6 +2,8 @@ package com.AutomationFramework.companyName.automationEcommerce.pageObject;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.AutomationFramework.companyName.automationEcommerce.helper.log4j.LoggerHelper;
@@ -10,16 +12,31 @@ import com.aventstack.extentreports.Status;
 
 public class Page_newProducts {
 
-
 	public WebDriver driver;
 	Logger log = LoggerHelper.getLogger(Page_newProducts.class);
 	
 	public Page_newProducts(WebDriver driver){
 		this.driver = driver;
-		
 		PageFactory.initElements(driver, this);
 		log.info("Page_newProducts Class Object Created");
 		TestBase.test.log(Status.INFO,"Page_newProducts Class Object Created");	
 	}
-
+	@FindBy(xpath = "//*[@id='columns']/div[1]/a")
+	public WebElement img_homeIcon;
+	
+	@FindBy(xpath = "//*[@id='columns']/div[1]/span[2]")
+	public WebElement lbl_newProducts1;
+	
+	@FindBy(xpath = "//*[@id='center_column']/h1")
+	public WebElement lbl_newProducts2;
+	
+	@FindBy(xpath = "//*[@id='center_column']/p")
+	public WebElement lbl_noNewProducts;
+	
+	public Page_index clkHomeIcon1() {
+		img_homeIcon.click();
+		log.info("Clicked on 'Home icon' button");
+		TestBase.test.log(Status.INFO, "Clicked on 'Home icon' button");
+		return new Page_index(driver);
+	}
 }
